ae0a85c4e1d127d407784ded663ce94351de2051
christophe <c.johanny@gmail.com>
2019-03-07 16:01:33 +0100

    first tests

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import {hot} from "react-hot-loader";
 import "./App.css"
 
 class App extends Component{
+  constructor() {
+    super()
+    this.state= {
+      counter: 0,
+      error: false
+    }
+    this.inc = this.inc.bind(this)
+    this.dec = this.dec.bind(this)
+  }
+
+  inc() {
+    if (this.state.error) {
+      this.setState({error: false, counter: this.state.counter + 1})
+    } else {
+      this.setState({counter: this.state.counter + 1})
+    }
+  }
+
+  dec() {
+    if (this.state.counter === 0) {
+      this.setState({error: true})
+    } else {
+      this.setState({counter: this.state.counter - 1})
+    }
+  }
+
   render(){
     return(
-      <div className="App">
-        <h1> Hello, World!! </h1>
+      <div data-test="component-app" className="App">
+        <h1 data-test="counter-display">the counter is currently {this.state.counter}</h1>
+        <button onClick={this.inc} data-test="increment-button">Increment Counter</button>
+        <button onClick={this.dec} data-test="decrement-button">Decrement Counter</button>
+        {this.state.error && <p data-test="error-message">you cannot decrement counter under 0</p>}
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,90 @@
 import React from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App'
 
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+const setup = (props={}, state= null) => {
+    const wrapper =  shallow(<App {...props}/>)
+    if (state) {
+        wrapper.setState(state)
+    }
+    return wrapper
+}
+
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test='${val}']`)
+}
+
 test('render whitout error', () => {
-    
+    const wrapper = setup()
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent.length).toBe(1)
+})
+
+test('render increment Button', () => {
+    const wrapper = setup()
+    const button = findByTestAttr(wrapper, 'increment-button')
+    expect(button.length).toBe(1)
+})
+
+test('render counter is displaying', () => {
+    const wrapper = setup()
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    expect(counterDisplay.length).toBe(1)
+})
+
+test('counter start at 0', () => {
+    const wrapper = setup()
+    const initialCounterState = wrapper.state('counter')
+    expect(initialCounterState).toBe(0)
+})
+
+test('increment the counter display when click', () => {
+    const counter = 7
+    const wrapper = setup(null, {counter})
+
+    const button = findByTestAttr(wrapper, 'increment-button')
+    button.simulate('click')
+    wrapper.update()
+
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    expect(counterDisplay.text()).toContain(counter + 1)
+})
+
+test('decrement the counter display when click', () => {
+    const counter = 8
+    const wrapper = setup(null, {counter})
+
+    const button = findByTestAttr(wrapper, 'decrement-button')
+    button.simulate('click')
+    wrapper.update()
+
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    expect(counterDisplay.text()).toContain(counter - 1)
+})
+
+test('should display error message when try to decrement counter at 0', () => {
+    const counter = 0
+    const wrapper = setup(null, {counter})
+
+    const button = findByTestAttr(wrapper, 'decrement-button')
+    button.simulate('click')
+    wrapper.update()
+
+    const errorMessage = findByTestAttr(wrapper, 'error-message')
+    expect(errorMessage.length).toBe(1)
+})
+
+test('should hide error message when inc again', () => {
+    const counter = 0
+    const wrapper = setup(null, {counter})
+
+    const button = findByTestAttr(wrapper, 'increment-button')
+    button.simulate('click')
+    wrapper.update()
+
+    const errorMessage = findByTestAttr(wrapper, 'error-message')
+    expect(errorMessage.length).toBe(0)
 })
