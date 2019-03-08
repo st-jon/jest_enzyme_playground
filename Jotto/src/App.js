a1b2a6b3ac63621815ba4fc23bf8ac82ae9989c6
christophe <c.johanny@gmail.com>
2019-03-08 11:05:56 +0100

    added jotto

---
+++ Jotto/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component} from "react"
+import {hot} from "react-hot-loader";
+import "./App.css"
+
+class App extends Component{
+  constructor() {
+    super()
+   
+  }
+
+
+  render(){
+    return(
+      <div data-test="component-app" className="App">
+        <h1 data-test="welcome-display">Welcome to Jotto</h1>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
---
+++ Jotto/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App'
+
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+const setup = (props={}, state= null) => {
+    const wrapper =  shallow(<App {...props}/>)
+    if (state) {
+        wrapper.setState(state)
+    }
+    return wrapper
+}
+
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test='${val}']`)
+}
+
+test('render whitout error', () => {
+    const wrapper = setup()
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent.length).toBe(1)
+})
